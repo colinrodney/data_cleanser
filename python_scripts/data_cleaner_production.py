58b3c255733cc38dc50ec4e41ceef9092ed85c62
colinrodney <bigboi0000@gmail.com>
2025-08-23 12:00:33 -0500

    Imported module: fill_missing_values into main file

--- python_scripts/data_cleaner_production.py
+++ python_scripts/data_cleaner_production.py
@@ -2,15 +2,19 @@ import load_data
 import clean_column_names
 import write_file_as_output
 import normalize_strings
+import fill_missing_values
 
 file_toBe_cleansed = load_data.load_file(r'C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_ORIGINAL.csv')
 print("FILE LOADED: \n\n ", file_toBe_cleansed, "\n")
 
 cleansed_file = clean_column_names.clean_column_names(file_toBe_cleansed)
-print("CLEANSED FILE: \n" ,cleansed_file, "\n")
+# print("CLEANSED FILE: \n" ,cleansed_file, "\n")
 
 cleansed_file = normalize_strings.normalize_strings(file_toBe_cleansed)
-print("CLEANSED FILE w/ strings normalized: \n" ,cleansed_file ,"\n")
+# print("CLEANSED FILE w/ strings normalized: \n" ,cleansed_file ,"\n")
+
+cleansed_file = fill_missing_values.fill_missing(file_toBe_cleansed)
+print("CLEANSED FILE w/ missing values filled: \n" ,cleansed_file ,"\n")
 
 write_file_as_output.output_file(cleansed_file)
 
