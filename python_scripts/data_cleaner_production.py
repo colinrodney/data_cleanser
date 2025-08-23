73d0d4ef2d4a3be4fe420f0c77987cef507c30b5
colinrodney <bigboi0000@gmail.com>
2025-08-23 07:12:03 -0500

    Imported module 'write_file_as_output'+ abstracting function code away from production file

--- python_scripts/data_cleaner_production.py
+++ python_scripts/data_cleaner_production.py
@@ -1,9 +1,13 @@
 import load_data
 import clean_column_names
+import write_file_as_output
 
 file_toBe_cleansed = load_data.load_file(r'C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_ORIGINAL.csv')
 print("FILE LOADED: \n\n ", file_toBe_cleansed, "\n")
 
 cleansed_file = clean_column_names.clean_column_names(file_toBe_cleansed)
 print(cleansed_file)
+
+write_file_as_output.output_file(cleansed_file)
+
 
