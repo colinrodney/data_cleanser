751c739942e9255457c4ece7a2440430c938eeb9
colinrodney <bigboi0000@gmail.com>
2025-08-22 20:45:02 -0500

    Abstracted function invocations away from their original files + placed them into data_cleaner_production.py

--- python_scripts/load_data.py
+++ python_scripts/load_data.py
@@ -31,8 +31,8 @@ print("FILE READ: \n\n ", df.info())
 
 # file = load_file(r"example_import.csv")
 # THIS WORKS BUT MIGHT BE REDUNDANT
-file_toBe_cleansed = load_file(r'C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_ORIGINAL.csv')
-print("FILE LOADED: \n\n ", file_toBe_cleansed)
+# file_toBe_cleansed = load_file(r'C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_ORIGINAL.csv')
+# print("FILE LOADED: \n\n ", file_toBe_cleansed)
 
 # Will this work > cleansed_file = df?
 # print(file)
