1467221ff0bf0cf5fd39b1188cf2be5bf0c39b7b
colinrodney <bigboi0000@gmail.com>
2025-09-09 06:36:19 -0500

    TRACKING ALL FILES - Preparing to switch branches

--- python_scripts/data_cleaner_production.py
+++ python_scripts/data_cleaner_production.py
@@ -3,6 +3,7 @@ import clean_column_names
 import write_file_as_output
 import normalize_strings
 import fill_missing_values
+import text_to_numeric
 
 file_toBe_cleansed = load_data.load_file(r'C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_ORIGINAL.csv')
 print("FILE LOADED: \n\n ", file_toBe_cleansed, "\n")
@@ -15,6 +16,9 @@ cleansed_file = normalize_strings.normalize_strings(file_toBe_cleansed)
 
 cleansed_file = fill_missing_values.fill_missing(file_toBe_cleansed)
 print("CLEANSED FILE w/ missing values filled: \n" ,cleansed_file ,"\n")
+
+cleansed_file = text_to_numeric.text_to_numeric(file_toBe_cleansed)
+print("CLEANSED FILE w/ numbers converted to numeric values: \n" ,cleansed_file ,"\n")
 
 write_file_as_output.output_file(cleansed_file)
 
