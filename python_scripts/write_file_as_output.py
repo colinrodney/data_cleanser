b040a6974b640e13f9f9e9f329b3e0b90d88291d
colinrodney <bigboi0000@gmail.com>
2025-10-04 14:41:52 -0500

    10/04/2025 - MAJOR CHANGE: FILES TRACKED IN THIS DIRECTORY TO BE USED FOR PORTFOLIO DEPLOYMENT ONLY! REFER TO DIRECTORY 'data_cleanser_NON_PROD' for notes / test files/test notebooks etc.

--- python_scripts/write_file_as_output.py
+++
@@ -1,9 +0,0 @@
-# WRITE CLEANSED FILE AS OUTPUT TO SPECIFIED PATH 
-''' note: cleansed file represented as df'''
-
-def output_file(df):
-    # cleansed_file.to_csv("concert_tours_by_women_TEST.csv")- DO NOT DELETE!
-    df.to_csv(r"C:\Users\Cessn\OneDrive\Desktop\sample_datasets\concert_tours_by_women\concert_tours_by_women_CLEANSED.csv")
-
-print("Cleaned data saved!")
-
